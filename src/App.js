ef28714df6632befb9311db932f28e61b1c17078
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2021-12-19 18:32:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React, {Suspense} from 'react';
-import Bar from './container/header';
-import Intro from "./container/intro";
-import About from "./container/about";
-import Service from "./container/service";
-import Portfolio  from "./container/portfolio";
-import Curriculum from "./container/curriculum";
-import Contact from "./container/contact";
 import Loader from "./component/loader";
-import './App.css';
 
 function App() {
   return (
     <Suspense fallback={<Loader />}>
-      
-      <Bar />
-      <Intro />
-      <About />
-      <Curriculum />
-      <Portfolio />
-      <Service />
-      <Contact />
      
     </Suspense>
   );
