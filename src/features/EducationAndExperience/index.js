2db734a4e68d9f018e01d24ec645fb81aa919a09
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2021-12-24 19:58:26 +0100

    update

--- src/features/EducationAndExperience/index.js
+++ src/features/EducationAndExperience/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useState } from "react";
 import { Typography } from "../../common/shared";
-import { BusinessCenter, School } from '@material-ui/icons';
+// import { BusinessCenter, School } from '@material-ui/icons';
 
 const BackgroundExperience = () => {
+  let [activeTab, setActtiveTab] = useState("carousel");
   let data = [
     {
       company: "Oracle Christian Center O2C",
@@ -10,7 +12,7 @@ const BackgroundExperience = () => {
       content:
         "Coordinate a team of technical sound engineers, to provide an excellent sound to the church",
       duration: "July 2014 - 2018 ",
-      icon: <BusinessCenter />
+      // icon: <BusinessCenter />
     },
     {
       company: "FIIRO",
@@ -18,7 +20,7 @@ const BackgroundExperience = () => {
       content:
         "Assisted with the processing of foodstuffs into granular and preserved state.",
       duration: "6 months",
-      icon: <BusinessCenter />
+      // icon: <BusinessCenter />
     },
     {
       company: "NIMET",
@@ -26,7 +28,7 @@ const BackgroundExperience = () => {
       content:
         "Assisted in reading, calculate and measuring the current state of weather and also predicting the weather outcomes.",
       duration: "05 May 2018 - 05 Oct 2018",
-      icon: <BusinessCenter />
+      // icon: <BusinessCenter />
     },
     {
       company: "Alabian Solutions",
@@ -34,14 +36,14 @@ const BackgroundExperience = () => {
       content:
         "Assisted in developing the company's client website and also design some website with figma",
       duration: "1 Sept 2021 - 20 Nov 2022",
-      icon: <BusinessCenter />
+      // icon: <BusinessCenter />
     },
     {
       company: "Federal University of Agriculture, Abeokuta",
       position: "Bachelor Degree",
       content: "Studied Physics in Federal University of Agriculture Abeokuta.",
       duration: "December 2014 - June 2018",
-      icon: <School />
+      // icon: <School />
     },
     // { company: "", position: "", content: "", duration: "" },
     // { company: "", position: "", content: "", duration: "" },
@@ -58,44 +60,29 @@ const BackgroundExperience = () => {
           Experience & <span className="text-green-400">Education</span>
         </Typography>
       </div>
-      <div className="grid grid-cols-1 gap-10 mt-14 px-20">
-        <ul className="overflow-x-auto w-full h-83 items-center flex gap-8 snap-x">
-          {data.map(({ company, position, content, duration, icon }, i) => (
-            <li key={i} className="snap-center">
-              <div className="shadow-lg rounded-xl h-52 w-72 max-w-[40vw] md:w-96 p-4 bg-white relative overflow-hidden">
-                <a href="#" className="w-full h-full block">
-                  <div className="flex items-center border-b-2 mb-2 py-2">
-                    <div className="h-10 w-10 bg-green-400 rounded-full"> {icon} </div>
-                    <div className="pl-3">
-                      <div className="font-medium">{company}</div>
-                      <div className="flex py-1">
-                        <div className="text-gray-600 text-sm">
-                          {position} -
-                        </div>
-                        <div className="text-gray-600 text-sm ml-1">
-                          {duration}
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-full p-4">
-                    <Typography
-                      capitalize
-                      variant={"body2"}
-                      color={"defaultBody"}
-                    >
-                      {content}
-                    </Typography>
-                  </div>
-                  <div className="w-full h-2 bg-green-200 rounded-full absolute bottom-2 right-1 left-1">
-                    <div className="w-full h-full text-center text-xs text-white bg-green-400 rounded-full"></div>
-                  </div>
-                </a>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="flex items-center justify-center mt-2">
+        <Typography
+          variant={"body1"}
+          className={`mx-1 ${
+            activeTab === "carousel" && "border-b-2 p-2 border-green-400"
+          } cursor-pointer`}
+          color={"white"}
+          onClick={() => setActtiveTab('carousel')}
+        >
+          Carousel
+        </Typography>
+        <Typography
+          variant={"body1"}
+          className={`mx-1 ${
+            activeTab === "sparsed" && "border-b-2 p-2 border-green-400"
+          } cursor-pointer`}
+          color={"white"}
+          onClick={() => setActtiveTab('sparsed')}
+        >
+          Scattered
+        </Typography>
       </div>
+      <div className="grid grid-cols-1 gap-10 mt-14 px-20"></div>
     </div>
   );
 };
---
+++ src/features/EducationAndExperience/layout/Slider.js
@@ -0,0 +1,40 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Typography } from "../../../common/shared";
+
+const SliderComponent = ({ data }) => {
+  return (
+    <ul className="overflow-x-auto w-full h-83 items-center flex gap-8 snap-x">
+      {data.map(({ company, position, content, duration, icon }, i) => (
+        <li key={i} className="snap-center">
+          <div className="shadow-lg rounded-xl h-52 w-72 max-w-[40vw] md:w-96 p-4 bg-white relative overflow-hidden">
+            <a href="#" className="w-full h-full block">
+              <div className="flex items-center border-b-2 mb-2 py-2">
+                <div className="h-10 w-10 bg-green-400 rounded-full">
+                  {" "}
+                  {icon}{" "}
+                </div>
+                <div className="pl-3">
+                  <div className="font-medium">{company}</div>
+                  <div className="flex py-1">
+                    <div className="text-gray-600 text-sm">{position} -</div>
+                    <div className="text-gray-600 text-sm ml-1">{duration}</div>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full p-4">
+                <Typography capitalize variant={"body2"} color={"defaultBody"}>
+                  {content}
+                </Typography>
+              </div>
+              <div className="w-full h-2 bg-green-200 rounded-full absolute bottom-2 right-1 left-1">
+                <div className="w-full h-full text-center text-xs text-white bg-green-400 rounded-full"></div>
+              </div>
+            </a>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default SliderComponent;
